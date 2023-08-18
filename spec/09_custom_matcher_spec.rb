602ec86208bb253753410587779364308371abf6
missopi <missopi@me.com>
2023-08-18 12:42:33 +0100

    Complete exercise so all tests pass

--- spec/09_custom_matcher_spec.rb
+++ spec/09_custom_matcher_spec.rb
@@ -37,28 +37,33 @@ describe 'one word palindrome test' do
   # Write a custom matcher that detects a one word palindrome,
   # using the following block: { |word| word.reverse == word }
   # When it is set up correctly, all of the following tests will pass.
-
-  context 'when a palindrome is used' do
-    # remove the 'x' before running this test
-    xit 'is a palindrome' do
-      expect(racecar).to be_a_palindrome
+  context 'palindrome matcher' do
+    matcher :be_a_palindrome do
+      match { |word| word.reverse == word }
     end
 
-    # remove the 'x' before running this test
-    xit 'is a palindrome' do
-      expect(rotator).to be_a_palindrome
-    end
-  end
+    context 'when a palindrome is used' do
+      # remove the 'x' before running this test
+      it 'is a palindrome' do
+        expect(racecar).to be_a_palindrome
+      end
 
-  context 'when a palindrome is not used' do
-    # remove the 'x' before running this test
-    xit 'is not a palindrome' do
-      expect(spaceship).not_to be_a_palindrome
+      # remove the 'x' before running this test
+      it 'is a palindrome' do
+        expect(rotator).to be_a_palindrome
+      end
     end
 
-    # remove the 'x' before running this test
-    xit 'is not a palindrome' do
-      expect(palindrome).not_to be_a_palindrome
+    context 'when a palindrome is not used' do
+      # remove the 'x' before running this test
+      it 'is not a palindrome' do
+        expect(spaceship).not_to be_a_palindrome
+      end
+
+      # remove the 'x' before running this test
+      it 'is not a palindrome' do
+        expect(palindrome).not_to be_a_palindrome
+      end
     end
   end
 end
