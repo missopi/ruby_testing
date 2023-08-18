256ac12fff9abe101df1a9279913be5d342bb457
missopi <missopi@me.com>
2023-08-18 16:46:55 +0100

    Complete assignment #5 & #6

--- spec/13_input_output_spec.rb
+++ spec/13_input_output_spec.rb
@@ -214,10 +214,11 @@ describe NumberGame do
     # Create a new instance of NumberGame, with specific values for @solution,
     # @guess, and @count
     context 'when count is 2-3' do
+      subject(:game_two) { described_class.new(5, '5', 3) }
       # remove the 'x' before running this test
-      xit 'outputs correct phrase' do
+      it 'outputs correct phrase' do
         congrats_phrase = "Congratulations! You picked the random number in 3 guesses!\n"
-        expect { game.final_message }.to output(congrats_phrase).to_stdout
+        expect { game_two.final_message }.to output(congrats_phrase).to_stdout
       end
     end
 
@@ -225,8 +226,11 @@ describe NumberGame do
 
     # Write a test for the following context.
     context 'when count is 4 and over' do
+      subject(:game_three) { described_class.new(5, '5', 7) }
       # remove the 'x' before running this test
-      xit 'outputs correct phrase' do
+      it 'outputs correct phrase' do
+        hard_phrase = "That was hard. It took you 7 guesses!\n"
+        expect { game_three.final_message }.to output(hard_phrase).to_stdout
       end
     end
   end
